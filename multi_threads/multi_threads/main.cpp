610d504a6991a6458e1308bc4abef9b7446ba1fc
Taras Kvitko <tkvitko@gmail.com>
2024-05-05 19:36:01 +0300

    Initial commit

---
+++ multi_threads/multi_threads/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  multi_threads
+//
+//  Created by tkvitko on 05.05.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
